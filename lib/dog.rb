e2481007effc40a3ca2a338c62b50ac6357f37dc
dolly-d <dolly.desir@gmail.com>
2020-06-01 16:45:06 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,12 @@
 class Dog
-    def bark
-        puts "Woof!"
-    end
-    def sit
-        puts "The Dog is sitting"
-    end
+
+def bark
+    puts "Woof!"
 end
 
-class Person
-    def talk
-        puts "Hello World!"
-    end
-    
-    def walk
-        puts "The Person is walking"
-    end
+def sit
+    puts "The Dog is sitting"
+end
+
+
 end
